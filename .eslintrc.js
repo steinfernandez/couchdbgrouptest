021406c6201b337c02dc6afdc37b90a2b3febd4e
Stein Astor Fernandez <saf6111@g.rit.edu>
2016-02-01 20:39:30 -0800

    ESLint config file and gitignore.

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    "rules": {
+		"no-console":0,
+        "indent": [
+            2,
+            0
+        ],
+        "quotes": [
+            2,
+            "double"
+        ],
+        "linebreak-style": [
+            2,
+            "unix"
+        ],
+    },
+    "env": {
+        "es6": true,
+        "node": true
+		},
+    "extends": "eslint:recommended"
+};
